4364254f4d0ef3d6f97d3d8c7d31c9a1768d23f8
Sam Robinson <robinson.will.sam@gmail.com>
2024-11-21 14:14:25 -0600

    working

--- sketch.js
+++ sketch.js
@@ -1,17 +1,33 @@
-let xOffset = 0;
-
+// let xOffset1 = 0;
+// let xOffset2 = 10000;
+let inc = 0.01
+let start = 0;
 function setup() {
   createCanvas(400, 400);
 
 }
 
 function draw() {
-  background(220);
-  
-  let x = map(noise(xOffset), 0, 1, 0, width);
-  xOffset += 0.01;
-  
-
-  ellipse(x, 200, 24, 24);
+  background(51);
+  stroke(255);
+  noFill();
+  beginShape();
+  let xOffset = start;
+  for (let x = 0; x < width; x++){
+    stroke(255);
+    let n = map(noise(xOffset), 0, 1, 0, height);
+    let s = map(sin(xOffset), -1, 1, -50, 50);
+    let y = s + n;
+    
+    
+    
+    
+    // let y = noise(xOffset) * height;
+    vertex(x, y);
+    xOffset += inc;
+  }
+  endShape();
+  start += inc;
+  // noLoop();
 }
 
